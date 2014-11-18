5326bff20b35ef647041823800249beaf38468f0
Ryan Artecona <ryanartecona@gmail.com>
2014-11-18 10:27:43 -0500

    rename SDInline to MDInline, invert parser monad transformer stack

--- src/Text/Markdown/SlamDown.purs
+++ src/Text/Markdown/SlamDown.purs
@@ -3,37 +3,40 @@ module SlamDown where
 import Text.Parsing.Parser
 import Text.Parsing.Parser.Combinators
 import Control.Monad.State.Trans
+import Control.Monad.Identity
 
 --------------------------------------------------------------------------------
 -- Types
 --------------------------------------------------------------------------------
 
 data MDBlock = HorizontalRule
-             | Header HLevel HUnderline SDInline
+             | Header HLevel HUnderline MDInline
              | Blockquote [MDBlock]
              | OrderedList StartIndex [MDBlock]
              | UnorderedList [MDBlock]
              | CodeBlock String
-             | Paragraph [SDInline]
+             | Paragraph [MDInline]
 
 data HLevel = H1 | H2 | H3 | H4 | H5 | H6
 data HUnderline = HU0 | HU1 | HU2
 type StartIndex = Number
 
-data SDInline = Plain String
+data MDInline = Plain String
               | Code CBInfo String
-              | Emphasized SDInline
-              | Strong SDInline
+              | Emphasized MDInline
+              | Strong MDInline
 
 newtype CBInfo = CBInfo String
 
 type MDDoc = [MDBlock] -- a markdown document is a list of MDBlocks
 
+
 -- Our main StateT Parser monad
 
 data MDPState = MDPState -- will become record of parser state
 
-type MDParser a = StateT MDPState (Parser String) a
+type MDParser a = ParserT String (StateT MDPState Identity) a
+
 
 --------------------------------------------------------------------------------
 -- Parsers

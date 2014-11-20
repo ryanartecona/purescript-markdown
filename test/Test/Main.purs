99bcfef570171f42bf84a7c334f847bc0537ce76
Ryan Artecona <ryanartecona@gmail.com>
2014-11-20 18:27:33 -0500

    add more tests for emph

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -11,5 +11,18 @@ parses p s t = case runMDParser p s of
                     Right a -> a == t
                       <?> "Expected: " ++ show t ++ "\n\n  Actual: " ++ show a ++ "\n\n"
 
+parseFails :: forall a. (Eq a, Show a) => MDParser a -> String -> Result
+parseFails p s = case runMDParser p s of
+                      Left _ -> Success
+                      Right e -> false
+                        <?> "Falsely parsed: " ++ show s ++ "\n  into: " ++ show e ++ "\n\n"
+
 main = do
   assert $ emph `parses` "*test*" $ Emphasized (Plain "test")
+  assert $ emph `parses` "*two words*" $ Emphasized (Plain "two words")
+  assert $ emph `parseFails` "*word"
+  assert $ emph `parseFails` "word*"
+  assert $ emph `parseFails` "* not adjacent*"
+  assert $ emph `parseFails` "* notadjacent*"
+  assert $ emph `parseFails` "*not adjacent *"
+  assert $ emph `parseFails` "*notadjacent *"
